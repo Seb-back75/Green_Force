e7020d49dbb05bdb855463824eee01fe663fd1ee
Seb-back75 <sebastien.l7516@gmail.com>
2020-01-13 17:34:44 +0100

    produits

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -109,7 +109,7 @@ class Produit
         return $this->image;
     }
 
-    public function setImage(string $image): self
+    public function setImage(?string $image): self
     {
         $this->image = $image;
 
