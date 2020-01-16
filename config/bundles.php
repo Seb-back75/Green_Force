fec436ffe6687dac5f240ea82594f6b263d60766
Eliza-dev <elisao.lyceehb@gmail.com>
2020-01-16 16:50:57 +0100

    debuginscription

--- config/bundles.php
+++ config/bundles.php
@@ -13,12 +13,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
-<<<<<<< HEAD
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    
-=======
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
->>>>>>> user_login_jq
 ];
