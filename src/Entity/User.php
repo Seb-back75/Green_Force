d00213c70ac265e61b07a8fd508f9d6b5fe4a91d
Eliza-dev <elisao.lyceehb@gmail.com>
2020-01-16 16:21:21 +0100

    inscription

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(
  *  fields={"email"},
- *  message="L'email que vous avez indiqué est déjà utilisé !"
+ *  message="L'email que vous avez indiqué est déjà utilisé !")
  */
 class User
 
@@ -25,41 +25,94 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Assert\Email()
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255)
      */
     private $entreprise;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Assert\Length(min="8",minMessage="Votre mot de passe doit faire minimum 8 caractères")
      */
-    private $password;
+    private $responsable;
 
     /**
-     * @Assert\EqualTo(propertyPath="confirm_password", message="Votre mot de passe doit etre le même que celui que vous confirmez")
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8",minMessage="Votre mot de passe doit faire minimum 8 caractères")
      */
-    public $confirm_password;
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="10",minMessage="Votre numero de téléphone doit faire minimum 10 caractères")
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8",minMessage="Votre mot de passe doit faire minimum 8 caractères")
+     */
+    private $mdp;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\EqualTo(propertyPath="confirm_mdp", message="Votre mot de passe doit etre le même que celui que vous confirmez")
+     */
+    public $confirm_mdp;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresseL;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $villeL;
+
+     /**
+     * @ORM\Column(type="integer")
+     */
+    private $CpL;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresseF;
+
+      /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $villeF;
+
+     /**
+     * @ORM\Column(type="integer")
+     */
+    private $CpF;
+
+      /**
+     * @ORM\Column(type="integer")
+     */
+    private $NTVA;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $NSIRET;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $NKBIS;
+
+    public function eraseCredentials() {}
+
+    public function getSalt() {}
+
+    public function getRoles() {
+        return ['ROLE_USER'];
+    }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
     }
 
     public function getEntreprise(): ?string
@@ -74,23 +127,176 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    public function getResponsable(): ?string
     {
-        return $this->password;
+        return $this->responsable;
     }
 
-    public function setPassword(string $password): self
+    public function setResponsable(string $responsable): self
     {
-        $this->password = $password;
+        $this->responsable = $responsable;
 
         return $this;
     }
 
-    public function eraseCredentials() {}
-
-    public function getSalt() {}
-
-    public function getRoles() {
-        return ['ROLE_USER'];
+    public function getEmail(): ?string
+    {
+        return $this->email;
     }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getConfirmMdp(): ?string
+    {
+        return $this->confirm_mdp;
+    }
+
+    public function setConfirmMdp(string $confirm_mdp): self
+    {
+        $this->confirm_mdp = $confirm_mdp;
+
+        return $this;
+    }
+
+    public function getAdresseL(): ?string
+    {
+        return $this->adresseL;
+    }
+
+    public function setAdresseL(string $adresseL): self
+    {
+        $this->adresseL = $adresseL;
+
+        return $this;
+    }
+
+    public function getVilleL(): ?string
+    {
+        return $this->villeL;
+    }
+
+    public function setVilleL(string $villeL): self
+    {
+        $this->villeL = $villeL;
+
+        return $this;
+    }
+
+    public function getCpL(): ?string
+    {
+        return $this->CpL;
+    }
+
+    public function setCpL(string $CpL): self
+    {
+        $this->CpL = $CpL;
+
+        return $this;
+    }
+
+    public function getAdresseF(): ?string
+    {
+        return $this->adresseF;
+    }
+
+    public function setAdresseF(string $adresseF): self
+    {
+        $this->adresseF = $adresseF;
+
+        return $this;
+    }
+
+    public function getVilleF(): ?string
+    {
+        return $this->villeF;
+    }
+
+    public function setVilleF(string $villeF): self
+    {
+        $this->villeF = $villeF;
+
+        return $this;
+    }
+
+    public function getCpF(): ?string
+    {
+        return $this->CpF;
+    }
+
+    public function setCpF(string $CpF): self
+    {
+        $this->CpF = $CpF;
+
+        return $this;
+    }
+
+    public function getNTVA(): ?string
+    {
+        return $this->NTVA;
+    }
+
+    public function setNTVA(string $NTVA): self
+    {
+        $this->NTVA = $NTVA;
+
+        return $this;
+    }
+
+    public function getNSIRET(): ?string
+    {
+        return $this->NSIRET;
+    }
+
+    public function setNSIRET(string $NSIRET): self
+    {
+        $this->NSIRET = $NSIRET;
+
+        return $this;
+    }
+
+    public function getNKBIS(): ?string
+    {
+        return $this->NKBIS;
+    }
+
+    public function setNKBIS(string $NKBIS): self
+    {
+        $this->NKBIS = $NKBIS;
+
+        return $this;
+    }
+
+    
+   
+
+   
 }
