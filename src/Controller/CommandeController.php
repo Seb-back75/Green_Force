85d68c2abc40136a8256b1d2fa867f343ee85f21
Eliza-dev <elisao.lyceehb@gmail.com>
2020-01-17 16:50:31 +0100

    panierajour

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\Commande;
 use App\Form\CommandeType;
 use App\Repository\CommandeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/commande")
@@ -16,79 +17,134 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommandeController extends AbstractController
 {
     /**
-     * @Route("/", name="commande_index", methods={"GET"})
+     * @Route("/panier", name="cart_index")
+     *  @Route("/", name="commande_index", methods={"GET"})
      */
-    public function index(CommandeRepository $commandeRepository): Response
-    {
+    public function panierCommande(SessionInterface $session,CommandeRepository $commandeRepository){
+        
+     
+          
+                $panier = $session->get('panier',[]);
+                $panierData = [];
+            foreach($panier as $id => $quantity){
+                $panierData[] = [
+                        'produit' => $commandeRepository->find($id),
+                        'quantity' => $quantity
+
+                ];
+                
+         }
+         
+         $total = 0;
+
+            foreach($panierData as $item){
+                $totalItem = $item['produit']->getTttc() * $item['quantity'];
+                $total += $totalItem;
+            }
+
+        
         return $this->render('commande/index.html.twig', [
-            'commandes' => $commandeRepository->findAll(),
+            
+            'items'=> $panierData,
+            'total'=> $total
         ]);
     }
 
-    /**
-     * @Route("/new", name="commande_new", methods={"GET","POST"})
+ /**
+     * @Route("/panier/add/{id}", name="cart_add")
      */
-    public function new(Request $request): Response
-    {
-        $commande = new Commande();
-        $form = $this->createForm(CommandeType::class, $commande);
-        $form->handleRequest($request);
+    public function add($id, SessionInterface $session){
+                
+            $panier = $session->get('panier',[]);
+            if(!empty($panier[$id])){
+                $panier[$id]++;
+            }else{
+            $panier[$id] = 1;
+            }
+            $session->set('panier', $panier);
+            return $this->redirectToRoute("commande_index");
+        
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($commande);
-            $entityManager->flush();
+    
+}
+/**
+ * @Route("/panier/remove/{id}", name="cart_remove")
+ */
+public function remove($id, SessionInterface $session) {
+            $panier = $session->get('panier',[]);
 
-            return $this->redirectToRoute('commande_index');
-        }
+            if(!empty($panier[$id])){
+                unset($panier[$id]);
+            }
 
-        return $this->render('commande/new.html.twig', [
-            'commande' => $commande,
-            'form' => $form->createView(),
-        ]);
-    }
+            $session->set('panier', $panier);
 
-    /**
-     * @Route("/{id}", name="commande_show", methods={"GET"})
-     */
-    public function show(Commande $commande): Response
-    {
-        return $this->render('commande/show.html.twig', [
-            'commande' => $commande,
-        ]);
-    }
+            return $this->redirectToRoute("commande_index");
+}
+//    /**
+//   * @Route("/new", name="commande_new", methods={"GET","POST"})
+//   */
+//     public function new(Request $request): Response
+//  {
+//          $commande = new Commande();
+//          $form = $this->createForm(CommandeType::class, $commande);
+//          $form->handleRequest($request);
 
-    /**
-     * @Route("/{id}/edit", name="commande_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Commande $commande): Response
-    {
-        $form = $this->createForm(CommandeType::class, $commande);
-        $form->handleRequest($request);
+//          if ($form->isSubmitted() && $form->isValid()) {
+//              $entityManager = $this->getDoctrine()->getManager();
+//              $entityManager->persist($commande);
+//              $entityManager->flush();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+//              return $this->redirectToRoute('commande_index');
+//          }
 
-            return $this->redirectToRoute('commande_index');
-        }
+//          return $this->render('commande/new.html.twig', [
+//              'commande' => $commande,
+//              'form' => $form->createView(),
+//          ]);
+//      }
 
-        return $this->render('commande/edit.html.twig', [
-            'commande' => $commande,
-            'form' => $form->createView(),
-        ]);
-    }
+     /**
+      * @Route("/{id}", name="commande_show", methods={"GET"})
+      */
+     public function show(Commande $commande): Response
+     {
+         return $this->render('commande/show.html.twig', [
+             'commande' => $commande,
+         ]);
+     }
 
-    /**
-     * @Route("/{id}", name="commande_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Commande $commande): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$commande->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($commande);
-            $entityManager->flush();
-        }
+     /**
+      * @Route("/{id}/edit", name="commande_edit", methods={"GET","POST"})
+      */
+     public function edit(Request $request, Commande $commande): Response
+     {
+         $form = $this->createForm(CommandeType::class, $commande);
+         $form->handleRequest($request);
 
-        return $this->redirectToRoute('commande_index');
-    }
+         if ($form->isSubmitted() && $form->isValid()) {
+             $this->getDoctrine()->getManager()->flush();
+
+             return $this->redirectToRoute('commande_index');
+         }
+
+         return $this->render('commande/edit.html.twig', [
+             'commande' => $commande,
+             'form' => $form->createView(),
+         ]);
+     }
+
+    // /**
+    //  * @Route("/{id}", name="commande_delete", methods={"DELETE"})
+    //  */
+    // public function delete(Request $request, Commande $commande): Response
+    // {
+    //     if ($this->isCsrfTokenValid('delete'.$commande->getId(), $request->request->get('_token'))) {
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->remove($commande);
+    //         $entityManager->flush();
+    //     }
+
+    //     return $this->redirectToRoute('commande_index');
+    // }
 }
